b797b93f8f3df48ea074f2022d74aa35c3789ac4
sujith-volyty <sujith.nara@volyty.com>
2025-05-16 15:19:05 +0530

    add user attribute - firstname

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -29,6 +29,11 @@ class User implements UserInterface
      */
     private $roles = [];
 
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $firstName;
+
     public function getId(): ?int
     {
         return $this->id;
@@ -110,5 +115,17 @@ class User implements UserInterface
     {
         // If you store any temporary, sensitive data on the user, clear it here
         // $this->plainPassword = null;
+    }
+
+    public function getFirstName(): ?string
+    {
+        return $this->firstName;
+    }
+
+    public function setFirstName(?string $firstName): self
+    {
+        $this->firstName = $firstName;
+
+        return $this;
     }
 }
